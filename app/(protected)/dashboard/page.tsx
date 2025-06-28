20b1eb02f607ce5601da39559f14351bc0546940
betselotT <betselottesfa9@gmail.com>
2025-06-29 00:14:22 +0300

    debugging for production

--- app/(protected)/dashboard/page.tsx
+++ app/(protected)/dashboard/page.tsx
@@ -1,4 +1,3 @@
-import { ChartAreaInteractive } from "@/components/chart-area-interactive";
 import WelcomeDashboard from "@/components/dashboard/welcome";
 import { SectionCards } from "@/components/section-cards";
 import { auth } from "@/lib/auth";
@@ -28,9 +27,6 @@ export default async function Page() {
       </div>
       <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
         <SectionCards />
-        <div className="px-4 lg:px-6">
-          <ChartAreaInteractive />
-        </div>
       </div>
     </div>
   );
